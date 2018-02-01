b941c40ee11b2c32940d2d4eb3ece01fb40da860
lambdaq <lambdaq@gmail.com>
2018-02-01 16:28:22 +0800

    debug worker_abspath

--- gnsq_mp/master.py
+++ gnsq_mp/master.py
@@ -32,6 +32,10 @@ class NsqMPController(object):
             raise ValueError('Empty lookupd_http_addresses')
         if not self.__class__.worker:
             raise NotImplementedError('need define worker script path')
+        self.worker_abspath = os.path.abspath(os.path.join(
+            sys.modules[self.__class__.__module__].__file__,
+            '..', self.__class__.worker))
+        print>>sys.stderr, ' [nsq_mp] worker: %s' % self.worker_abspath
         self.lookupds = cycle(Lookupd(x) for x in self.__class__.lookupd_http_addresses)
 
     @property
@@ -59,13 +63,10 @@ class NsqMPController(object):
             # or the spawned subprocess exited, .poll() has a returnvalue
             if proc is None or proc.poll() is not None:
                 proc = subprocess.Popen([
-                    sys.executable, os.path.abspath(
-                        os.path.join(
-                            sys.modules[self.__class__.__module__].__file__,
-                            '..', self.__class__.worker)),
+                    sys.executable, self.worker_abspath,
                     self.topic, nsqd_tcp_addr, self.channel_name])
                 ProcessAffinity(proc.pid).affinity = i
-                print>>sys.stderr, ' [start] pid=%s, nsqd=%s' % (proc.pid, nsqd_tcp_addr)
+                print>>sys.stderr, ' [spawn] pid=%s, nsqd=%s' % (proc.pid, nsqd_tcp_addr)
                 self._procs[nsqd_tcp_addr] = proc
         self.poll_count += 1
 
@@ -80,7 +81,7 @@ class NsqMPController(object):
             for k, p in inst._procs.iteritems():
                 p.send_signal(2)
                 p.wait()
-                print>>sys.stderr, ' [stop] %s pid=%s, rc=%d' % (k, p.pid, p.returncode)
+                print>>sys.stderr, ' [kill] %s pid=%s, rc=%d' % (k, p.pid, p.returncode)
             print>>sys.stderr, ' [exit] all subprocess stopped.'
             exit(0)
         return inst
